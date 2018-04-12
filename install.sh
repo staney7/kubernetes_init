3f688f8c5ae4e8e818f106da8190f52e7575efe9
staney7 <smt_xj@sina.com>
2018-04-12 21:41:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ set -x
 USER=ubuntu # 用户
 GROUP=ubuntu # 组
 FLANELADDR=https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
-KUBECONF=/home/ubuntu/kubeadm.conf # 文件地址, 改成你需要的路径
-REGMIRROR=YOUR_OWN_DOCKER_REGISTRY_MIRROR_URL # docker registry mirror 地址
+KUBECONF=/home/ubuntu/kubernetes_init/kubeadm.conf # 文件地址, 改成你需要的路径
+REGMIRROR=https://docker.mirrors.ustc.edu.cn/ # docker registry mirror 地址
 
 # you can get the following values from `kubeadm init` output
 # these are needed when creating node
